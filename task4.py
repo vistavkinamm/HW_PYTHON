eb896bb04adb5e4b19624765b8b445489ce4a4d5
Marina Vystavkina <vistavkinamm@mail.ru>
2022-11-01 16:25:13 +0300

    task4

---
+++ task4.py
@@ -0,0 +1,17 @@
+# Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных позициях. 
+# Позиции хранятся в файле file.txt в одной строке одно число.
+
+from random import randint
+
+def list(n):
+    list = []
+    for i in range(n):
+        list.append(randint(-n, n))
+    return list
+
+n = int(input('Введите число N: '))
+numbers = list(n)
+print(numbers)
+x = open('file.txt','r')
+result = numbers[int(x.readline())] * numbers[int(x.readline(2))]
+print(result)
